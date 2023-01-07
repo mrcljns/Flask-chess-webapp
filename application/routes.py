7a97acc3abc397b6e47ba28140f34691b65e881d
mlorens <macieklorens@gmail.com>
2023-01-07 17:41:02 +0100

    Added more functionalities

--- application/routes.py
+++ application/routes.py
@@ -1,34 +1,76 @@
-from application import app
-from flask import render_template, request, url_for
+from application import app, db, bcrypt
+from flask import render_template, request, url_for, flash, redirect
+from application.forms import RegistrationForm, LoginForm, GameForm
+from flask_login import login_user, current_user, logout_user, login_required
+from application.models import User, Game
 import pandas as pd
 import json
 import plotly
 import plotly_express as px
-import sqlite3
+
 
 @app.route("/")
+@app.route("/home")
 def index():
     return render_template("index.html", title = "Home")
 
-@app.route("/dashboard", methods=['GET', 'POST'])
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Your account has been created! Welcome to ChesStats!", "success")
+        return redirect(url_for("login"))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route("/dashboard")
+@login_required
 def dashboard():
-    with open('data/query1.sql', 'r') as sql_file:
-        sql_script = sql_file.read()
+    return render_template('dashboard.html', title='Dashboard')
 
-    connection = sqlite3.connect('data/rating_ver2.db')
 
-    openings_df = pd.read_sql_query(sql_script, connection)
-
-    names = sorted(list(set(openings_df['name'])))
-
-    selected_gm = request.form.get('selected-gm')
-
-    # Graphs
-    fig1 = px.bar(openings_df.query('name == @selected_gm'), x="Name:1", y='opening_count', title=f"{selected_gm} openings")
-
-    graphlJSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
-
-    connection.commit()
-    connection.close()
-
-    return render_template("index.html", title = "Dashboard", names=names, graphlJSON=graphlJSON, selected_gm=selected_gm)
+@app.route("/game/new", methods=['GET', 'POST'])
+@login_required
+def add_game():
+    form = GameForm()
+    if form.validate_on_submit():
+        game = Game(player=current_user.username, piece_color=form.piece_color.data, result=form.result.data, moves=form.moves.data)
+        db.session.add(game)
+        db.session.commit()
+        flash('You have added a game!', 'success')
+        return redirect(url_for('index'))
+    return render_template('add_game.html', title='Add Game', form=form, legend='Add Game')
