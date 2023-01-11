7f2e84b07edc4b21f82b5dd008b509298424886c
polaparol1 <84683215+polaparol1@users.noreply.github.com>
2023-01-11 22:00:09 +0100

    Add files via upload

--- application/models.py
+++ application/models.py
@@ -12,6 +12,9 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    year_of_birth = db.Column(db.Integer, nullable=False)
+    elo = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(10), nullable=False)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
--- application/routes.py
+++ application/routes.py
@@ -1,13 +1,13 @@
 from application import app, db, bcrypt
 from flask import render_template, request, url_for, flash, redirect
-from application.forms import RegistrationForm, LoginForm, GameForm
+from application.forms import RegistrationForm, LoginForm, GameForm, UpdateAccountForm, PlayerForm
 from flask_login import login_user, current_user, logout_user, login_required
 from application.models import User, Game
 import pandas as pd
 import json
 import plotly
 import plotly_express as px
-
+import sqlite3
 
 @app.route("/")
 @app.route("/home")
@@ -24,7 +24,7 @@ def register():
 
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        user = User(username=form.username.data, email=form.email.data, year_of_birth=form.year_of_birth.data, elo=form.elo.data, title=form.title.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash("Your account has been created! Welcome to ChesStats!", "success")
@@ -74,3 +74,35 @@ def add_game():
         flash('You have added a game!', 'success')
         return redirect(url_for('index'))
     return render_template('add_game.html', title='Add Game', form=form, legend='Add Game')
+
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.elo = form.elo.data
+        current_user.title = form.title.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.elo.data = current_user.elo
+        form.title.data = current_user.title
+    return render_template('account.html', title='Account', form=form)
+
+@app.route("/board", methods=['GET', 'POST'])
+@login_required
+def board():
+    form = PlayerForm()
+    player = ''
+    if form.validate_on_submit():
+        player = form.player.data.upper()
+    con = sqlite3.connect("/Users/polaparol/Documents/DS/Python/Flask-chess-webapp-main/instance/chessgames.db")
+    cur = con.cursor()
+    games = cur.execute(f'SELECT * FROM games WHERE To_show is not null AND (upper(White) like "{player}%" or  upper(Black) like "{player}%") LIMIT 20')
+    return render_template('board.html', title='Chessboard', form=form, player=player, games=games)
