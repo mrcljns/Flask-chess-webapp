d89f4eeabf38a11197bae064d1b062532f7c9f50
mlorens <macieklorens@gmail.com>
2023-02-12 18:05:01 +0100

    Player rating changes with added game

--- application/__init__.py
+++ application/__init__.py
@@ -4,10 +4,6 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bce2711492820cc80963590f8c93953c'
-<<<<<<< HEAD
-=======
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chessdb.db'
->>>>>>> 541bba07f035b699ccbcce133fd5098240c8b29d
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
